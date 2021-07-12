e9e951b2fd529d482dd43fb0cf9efabb58e523ef
william <willhgreen02@gmail.com>
2021-07-12 13:47:06 -0400

    commit tree bc5b481449c54dd7f272ff40b1d158915aa918d2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride<='400')
+  return 'This one is on me!'
+  if(ride=>'2000 feet')
+  return 'I will gladly take your thirty bucks.'
+  if (ride=>'2500 feet')
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city ==='NYC'?"Ok, sounds good." : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+    }
+  }
